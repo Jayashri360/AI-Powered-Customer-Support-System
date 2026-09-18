d280fdb425e84b7bb162dbc3a7fd16bdf4fed119
jayashri360 <jayashri3600@gmail.com>
2026-09-18 19:23:43 +0800

    Add project files.

---
+++ CustomerSupport.Application/Interfaces/Repositories/ITicketRepository.cs
@@ -0,0 +1,11 @@
+﻿using CustomerSupport.Domain.Entities;
+namespace CustomerSupport.Application.Interfaces.Repositories
+{
+    public interface ITicketRepository
+    {
+        Task<SupportTicket?> GetByIdAsync(int id, bool trackChanges);
+        Task AddAsync(SupportTicket ticket);
+        Task SaveChangesAsync();
+    }
+}
+
